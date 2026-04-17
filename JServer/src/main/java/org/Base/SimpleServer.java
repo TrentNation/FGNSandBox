1f155de2b7f96abbc9e23d8e58f2d1a359d95dac
FGNation <trentsdatabase@gmail.com>
2026-04-17 13:07:20 -0500

    Major Changes: Modify System to change how the System works. Wanted to implement a dual System that handles both HTTP requests and Socket Request. Plans for HTTP are apparent but not much for the Socket Clients.

--- JServer/src/main/java/org/Base/SimpleServer.java
+++ JServer/src/main/java/org/Base/SimpleServer.java
@@ -6,57 +6,107 @@ import java.util.Scanner;
 
 public class SimpleServer {
 	public static void main(String[] args) throws IOException {
-		final int PORT = 8080;
-		ServerSocket serverSocket = new ServerSocket(PORT);
-		System.out.println("SERVER STARTED\nPort:" + PORT);
-		while (true) {
-			//Block and Wait for a client to connect
-			Socket clientSocket = serverSocket.accept();
-			System.out.println("Client CONNECTED" + clientSocket.getInetAddress());
+		final int httpPort = 8080;
+		final int socketPort = httpPort + 1010;
+		//Starts Both Servers on separate Threads
+		new Thread(() -> startServerWeb(httpPort)).start();
+		new Thread(() -> startServerSocket(socketPort)).start();
 
-			//Handle the Client in a new thread(Allowing us to serve multiple clients)
-			new Thread(() -> handleClient(clientSocket)).start();
-			new Thread(() -> handleClientWebPage(clientSocket)).start();
-		}
+		//Feedback to System
+		System.out.println("HTTP Server started on Port: " + httpPort);
+		System.out.println("Socket Server started on Port: " + socketPort);
+
 	}
 
-	static void handleClient(Socket socket) {
-		try {
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(socket.getInputStream()));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			String message;
-			while ((message = in.readLine()) != null) {
-				System.out.println("Received: " + message);
-				out.println("Echo:  " + message);
+
+
+	/**
+	 * Purpose: Handles The Server Function for Web Pages.
+	 * @param PORT : Holds the Main Server Port Number
+	 */
+	static void startServerWeb(int PORT){
+		try(ServerSocket server = new ServerSocket(PORT)){
+			System.out.println("SERVER STARTED");
+			while (true){
+				Socket webClient = server.accept();
+				new Thread(() -> handleClientWebPage(webClient)).start();
 			}
-
-		} catch (IOException io) {
-			io.printStackTrace();
+		}catch (IOException io){
+			System.out.println("Web Server ERROR: " + io.getMessage());
 		}
 	}
 
-	//For the purpose of Handling WebPage Responses
-	static void handleClientWebPage(Socket socket) {
-		try (
+	/**
+	 * Purpose: The WebPage Section
+	 * Future Plans: Opens an HTML file for the User
+	 * @param socket
+	 */
+	 static void handleClientWebPage(Socket socket) {
+	try(BufferedReader in = new BufferedReader(
+			new InputStreamReader(socket.getInputStream()));
+	    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	){
+		System.out.println("Are...we...connected? " + socket.getInetAddress());
+
+		String line;
+		while(!(line = in.readLine()).isEmpty()){
+			out.println(line);
+		}
+		//Sends an HTTP Response
+		String body = "<html><body><h1>We Are Here, Together!</h1></body></html>";
+		out.println("HTTP/1.1 200 OK");
+		out.println("Content-Type: text/html");
+		out.println("Content-Length: " + body.length());
+		out.println(body);
+	} catch (IOException io) {
+		System.out.println("Client ERROR: " + io.getMessage());
+	}
+	}
+
+	/**
+	 * Purpose: Handles The Server Function for Raw Sockets
+	 * @param PORT
+	 */
+	static void startServerSocket(int PORT){
+		 try( ServerSocket server = new ServerSocket(PORT);)
+		 {
+			 System.out.println("Server STARTED");
+			 while (true){
+				 Socket clientSocket = server.accept();
+				 new Thread (() -> handleClientSocket(clientSocket)).start();
+			 }
+
+		 }catch (IOException io){
+			 System.out.println("Server ERROR: " + io.getMessage());
+		 }
+	}
+
+	/**
+	 * Purpose: The Socket Function
+	 * Future Ideas: Maybe System Details?
+	 * @param socket
+	 */
+	static void handleClientSocket( Socket socket) {
+		try(
 				BufferedReader in = new BufferedReader(
 						new InputStreamReader(socket.getInputStream()));
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		) {   //Reading the HTTP request
-			String line;
-			while (!(line = in.readLine()).isEmpty()) {
-				System.out.println("Received: " + line);
+				)
+				 {
+
+
+			//Send a Socket response
+			String message;
+			while ((message = in.readLine()) != null) {
+				System.out.println("Received: " + message); //System Message
+				out.println("Echo: " + message); //Reponse Message
 			}
-			//Send an HTTP response
-			String body = "<html><body><h1>We Are Here, Together!</h1></body></html>";
-			out.println("HTTP/1.1 200 OK");
-			out.println("Content-Type: text/html");
-			out.println("Content-Length: " + body.length());
-			out.println(body);
+
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Client ERROR: " + e.getMessage());
 		}
 	}
+
 	static void uploadWebPage(){
 		System.out.println("Uploading Web Page.");
 	}
