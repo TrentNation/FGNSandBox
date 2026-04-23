3cc024158f11f779f5623731010b0e1b0b84a427
FGNation <trentsdatabase@gmail.com>
2026-04-23 01:59:59 -0500

    SimpleServer: Adding way to Upload WebPages

--- JServer/src/main/java/org/Base/SimpleServer.java
+++ JServer/src/main/java/org/Base/SimpleServer.java
@@ -2,6 +2,9 @@ package org.Base;
 
 import java.net.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class SimpleServer {
@@ -26,7 +29,7 @@ public class SimpleServer {
 	 */
 	static void startServerWeb(int PORT){
 		try(ServerSocket server = new ServerSocket(PORT)){
-			System.out.println("SERVER STARTED");
+			System.out.println("Web Server STARTED");
 			while (true){
 				Socket webClient = server.accept();
 				new Thread(() -> handleClientWebPage(webClient)).start();
@@ -48,16 +51,31 @@ public class SimpleServer {
 	){
 		System.out.println("Are...we...connected? " + socket.getInetAddress());
 
-		String line;
-		while(!(line = in.readLine()).isEmpty()){
-			out.println(line);
+		String line = in.readLine(); //To read the Header & Check the file
+
+		while(!(in.readLine()).isEmpty()){
+			out.println(line); //FIller for now
 		}
-		//Sends an HTTP Response
-		String body = "<html><body><h1>We Are Here, Together!</h1></body></html>";
-		out.println("HTTP/1.1 200 OK");
-		out.println("Content-Type: text/html");
-		out.println("Content-Length: " + body.length());
-		out.println(body);
+		if (line == null) return;
+		String path = line.split(" ")[1];
+
+		//If root was requested
+		if (path.equals("/")) path = "/index.html";
+		Path filePath = Path.of("resources" + path);
+		if (Files.exists(filePath)) {
+			//Sends an HTTP Response
+			String body = Files.readString(filePath);
+			out.println("HTTP/1.1 200 OK");
+			out.println("Content-Type: text/html");
+			out.println("Content-Length: " + body.length());
+			out.println(body);
+		}
+		else {
+			String body = "<html> <body><h1> 404 File Not Found </h1></body></html";
+			out.println("HTTP/1.1 404 Not Found");
+			out.println("Content-Type: text/html");
+			out.print(body);
+		}
 	} catch (IOException io) {
 		System.out.println("Client ERROR: " + io.getMessage());
 	}
@@ -70,7 +88,7 @@ public class SimpleServer {
 	static void startServerSocket(int PORT){
 		 try( ServerSocket server = new ServerSocket(PORT);)
 		 {
-			 System.out.println("Server STARTED");
+			 System.out.println("Socket Server STARTED");
 			 while (true){
 				 Socket clientSocket = server.accept();
 				 new Thread (() -> handleClientSocket(clientSocket)).start();
