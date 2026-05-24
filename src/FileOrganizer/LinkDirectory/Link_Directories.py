8d78feef5ba16011e04dcbb6fc06efb739fed7b1
FGNation <trentsdatabase@gmail.com>
2026-05-24 05:11:16 -0500

    File Organizer: Link_Directories

---
+++ src/FileOrganizer/LinkDirectory/Link_Directories.py
@@ -0,0 +1,10 @@
+#Import os Library
+import os
+
+# Source file path
+src = "/C/User/file.txt"
+
+#Create a copy of the file above
+# Destination file path
+dst = "/Code/Python/file.txt"
+os.link(src, dst)
