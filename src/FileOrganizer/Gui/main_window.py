971e6a24290ff88d2bd7bb9e7e81ae9169be89ff
FGNation <trentsdatabase@gmail.com>
2026-09-08 17:01:34 -0400

    File Organizer: Sort Screen

--- src/FileOrganizer/Gui/main_window.py
+++ src/FileOrganizer/Gui/main_window.py
@@ -18,9 +18,17 @@ Main Idea:
 "Clicking on them switches to their directory"
 "You can back up a file via another button"
 '''
-def switch_window():
+def switch_window(screen=None):
     None
 
+def sort_screen(screen : tk.Tk() = None):
+    new_window = tk.Toplevel()
+    new_window.title("Sorting Screen")
+    new_window.geometry("100x100")
+    frame_sort = tk.Frame(new_window)
+    frame_sort.pack(padx = 5, pady = 5, fill="x")
+
+    None
 def add_task(event = None):
     task = entry_task.get().strip()
     if task:
@@ -45,22 +53,25 @@ if __name__ == "__main__":
 
     frame_main = tk.Frame(root)
     frame_main.pack(padx=10, pady=10, fill="x")
-
+    '''
     entry_task = tk.Entry(frame_main, font=("Arial", 12))
     entry_task.pack(side="left", fill="x", expand=True, padx= (0, 5))
     entry_task.bind("<Return>", add_task) #Pressing Enter adds the task
 
     button_add = tk.Button(frame_main, text ="Add", command=add_task, width=8)
     button_add.pack(side="right")
+    '''
 
     #Options Section
     option_list = tk.Frame(root)
     option_list.pack(side="right",padx=10,expand=True, pady=10,fill="both")
     option_list.config(highlightthickness=0.5, highlightbackground="black")
 
-    button_add = tk.Button(option_list, text="Sort Folders" ,command = switch_window, font=("Arial", 20))
-    button_add.pack(side="left",expand = True)
+    button_Sort = tk.Button(option_list, text="Sort Folders" ,command = sort_screen, font=("Arial", 20))
+    button_Sort.grid(row=0, column = 0, padx = 5, pady = 5)
 
+    button_Link = tk.Button(option_list, text ="Link Folders", command = switch_window, font=("Arial",20))
+    button_Link.grid(row=1, column=0, padx = 5, pady = 5)
 
 
     # Viewing Folders Section
