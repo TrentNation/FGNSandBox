03191e3fe09e2389036d1adf8f08043e888ddafb
FGNation <trentsdatabase@gmail.com>
2026-09-11 21:13:55 -0500

    File Organizer: Sort Screen: Action Options

--- src/FileOrganizer/Gui/main_window.py
+++ src/FileOrganizer/Gui/main_window.py
@@ -24,9 +24,10 @@ def switch_window(screen=None):
 def sort_screen():
     new_window = tk.Toplevel()
     new_window.title("Sorting Screen")
-    new_window.geometry("400x400")
+    new_window.geometry("500x400")
     new_window.config(background="#907948")
 
+    current_directory = os.getcwd()
     frame_sort = tk.Frame(new_window)
     frame_sort.pack(padx = 10, pady = 10, fill="both",expand=True)
     frame_sort.config(highlightthickness=0.5, highlightbackground="black")
@@ -36,18 +37,40 @@ def sort_screen():
     frame_current_directory.pack(side = "left", ipadx = 10, ipady = 10, expand=True,fill="both")
     frame_current_directory.config(highlightthickness=0.5, highlightbackground="blue", width=100)
 
+    listbox_directory_files = tk.Listbox(frame_current_directory)
+    listbox_directory_files.pack(side = "left",expand = True, fill="both" )
+    listbox_directory_files.insert(tk.END, )
+
     #Current Directory Name
+    '''
+    Missing :
+        -Updatable Directory Name
+    '''
     frame_directory_name = tk.LabelFrame(frame_sort)
     frame_directory_name.config(highlightthickness=0.5, highlightbackground="red", height=50, width = 100, text="Current Directory:")
     frame_directory_name.pack( fill="both", expand=True)
-    text_directory_name = tk.Label(frame_directory_name,font=("Arial", 14), text="Yoooooooooooooooooooooooooooooooooooooooooo", wraplength=200)
+    text_directory_name = tk.Label(frame_directory_name,font=("Arial", 14), text=current_directory.split('\\')[-1], wraplength=200)
     text_directory_name.pack(expand=True, fill="both")
 
     #Action Options
-    frame_options = tk.Frame(frame_sort)
-    frame_options.pack(ipadx=10, ipady=300, expand = True, side = "left", fill="both")
-    frame_options.config(highlightthickness=0.5, highlightbackground="green", width=100)
     '''
+    Missing:
+        -Wrapping Around Options
+        -Actionable Buttons
+    '''
+    frame_options = tk.Frame(frame_sort)
+    frame_options.pack(ipadx=10, ipady=300, expand = True, fill="both")
+    frame_options.config(highlightthickness=0.5, highlightbackground="green", width=100)
+
+    button_sort_folder = tk.Button(frame_options, text="Sort Folders", font = ("Arial", 20))
+    button_change_directory = tk.Button(frame_options, text="Change Current Directory", font = ("Arial", 20))
+    button_output_change = tk.Button(frame_options, text = "Change Output Folders", font = ("Arial", 20))
+    button_sort_folder.grid(row = 0,column=0, sticky="nsew")
+    button_change_directory.grid(row=1,column=0, sticky="nsew")
+    button_output_change.grid(row=2,column=0, sticky="nsew")
+
+
+'''
 
 def add_task(event = None):
     task = entry_task.get().strip()
@@ -109,6 +132,7 @@ if __name__ == "__main__":
 
     #button_delete = tk.Button(root, text="Delete Selected Task", command = delete_task, background = "tomato", fg = "white")
     #button_delete.pack(fill = "x", padx=10, pady=10)
+
     #default_path = "C:\Coding\Coding Projects\Python Projects\Practice\FileOrganizer\src\FileOrganizer"
     #text_test_list =  os.listdir(default_path)
 
---
+++ src/FileOrganizer/PersonalizeDirectory/personalize_controller.py
@@ -0,0 +1,12 @@
+'''
+Idea Folder for "Personalize Directories:
+Renaming Conventions:
+    -The ability to rename certain files IF they follow a certain naming convention:
+        E.X. : GAMEDATA99, GAMEDATA98, and GAMEDATA01 all share the same naming convention of "GAMEDATA"
+            -> Selecting these type of files will allow you to customize how they are named:
+                -> Asdension(No skips?) ---> GAMEDATA00, GAMEDATA01, GAMEDATA02
+                -> Descension(No skips?) ---> GAMEDATA99, GAMEDATA98, GAMEDATA98
+                -> Named after X: GAMEDATAaugust, etc.
+
+
+'''
